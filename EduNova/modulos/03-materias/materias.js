7bc9bf139ea2a83a12d712fd6957168d897f0359
moperezar-web <moperezar@ucvvirtual.edu.pe>
2026-05-20 19:30:21 -0500

    Delete EduNova directory

--- EduNova/modulos/03-materias/materias.js
+++
@@ -1,7 +0,0 @@
-document.querySelectorAll('.tarjeta-materia').forEach(tarjeta => {
-  tarjeta.addEventListener('click', function() {
-    const nombreMateria = this.querySelector('h3').textContent;
-    console.log(`Abriendo contenido de: ${nombreMateria}`);
-    alert(`Entrando al aula virtual de: ${nombreMateria}`);
-  });
-});
